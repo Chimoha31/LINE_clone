66a81286a04716d86c3e3d4a95182a8c0d601a72
Chimoha31 <mokochii1108@gmail.com>
2022-03-01 21:01:48 -0800

    styling

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {db, auth} from '../firebase';
 import firebase from 'firebase/compat/app';
 import { Input } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 // inputに入れたtextをuseState()の変数messageに入れ、それをfirestoreのdatabaseに保存していくという処理
 function SendMessage() {
@@ -31,11 +32,21 @@ function SendMessage() {
       <form onSubmit={sendMessage}>
         <div class="sendMsg">
           <Input
+          style={{
+            width: "78%",
+            fontSize: "15px",
+            fontWeight: "550",
+            marginLeft: "5px",
+            marginBottom: "-3px",
+          }}
             placeholder="type message here"
             type="text"
             onChange={(e) => setMessage(e.target.value)}
             value={message}
           />
+          <SendIcon style={{
+            color: "#7AC2FF", marginLeft: "20px"
+          }}/>
         </div>
       </form>
     </div>
